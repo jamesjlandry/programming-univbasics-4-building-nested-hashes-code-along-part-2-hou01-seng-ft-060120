cbac08d686693a7cec07e15ee39173d86450fbec
James Landry <james.j.landry@outlook.com>
2020-05-19 21:03:06 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -20,9 +20,11 @@ def update_status
       }
    },
 
-epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead",
 
-epic_tragedy[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead",
+  
+  return epic_tragedy[:montague][:hero][:status] && epic_tragedy[:capulet][:heroine][:status]
 
 end
 
